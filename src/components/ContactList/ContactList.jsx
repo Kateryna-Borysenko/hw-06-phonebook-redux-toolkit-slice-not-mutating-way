b3085afe796e989ae5f2236cba2c0b4a8ab3102c
Kateryna <k.borysenko.kyiv@gmail.com>
2022-02-06 17:32:00 +0200

    Fixed filter bug

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo} from 'react';
+import { useContext, useMemo, useEffect} from 'react';
 import { useSelector, useDispatch} from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { contactsActions } from 'redux/contacts';
@@ -15,12 +15,21 @@ const ContactList = () => {
   
   const { t } = useTranslation();
 
+  
   const filteredContacts = useMemo(() => {
     
-  return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()),
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()),
     );
-
+    
   },[contacts, filter]);
+
+  useEffect(() => {
+    if (filteredContacts.length === 0) {
+      
+      dispatch(contactsActions.changeFilter('')); 
+    }
+  
+  }, [filteredContacts.length, dispatch]);
 
   return (
     <ul className={s.contactList}>
